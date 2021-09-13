f86215bcd99ef449dc91c30d458bf8d6b951a278
ManasaMamidoju <manasa.mamidojuofficial@gmail.com>
2021-09-12 23:16:21 -0400

    Flixster Part 2 Basic Structure Completed

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,20 +3,40 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
+    double rating;
+    int movieID;
+
+    //This is the empty constructor needed by the Parceler method
+    public Movie(){
+
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieID() {
+        return movieID;
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieID = jsonObject.getInt("id");
+
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
